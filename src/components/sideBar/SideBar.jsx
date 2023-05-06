19bf0c5ddc50ef615437fb89c09de41a64140fe4
GoktugYmsk <gogtug033@outlook.com>
2023-05-06 11:48:40 +0300

    simple bug fixed

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -7,9 +7,9 @@ function SideBar() {
         <aside>
             <h2>Side Bar Başlığı</h2>
             <ul>
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
+                <li><a href="#">Proje 1</a></li>
+                <li><a href="#">Proje 2</a></li>
+                <li><a href="#">Proje 3</a></li>
             </ul>
         </aside>
     );
